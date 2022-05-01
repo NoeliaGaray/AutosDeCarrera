cd4a57a065b84a177b1d74537c7325ee6109ba02
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-05-01 01:14:19 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+rotulo1.innerHTML = "Primera vuelta";
+rotulo2.innerHTML = "Segunda vuelta";
+rotulo3.innerHTML = "Tercera vuelta";
+rotulo4.innerHTML = "Cuarta vuelta";
+
+let btnEnv = document.getElementById("btnEnviar");
+let vuelta1: number = Number(dato1.value);
+let vuelta2: number = Number(dato2.value);
+let vuelta3: number = Number(dato3.value);
+let vuelta4: number = Number(dato4.value);
+let resultado: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+let promedio: number = resultado / 4;
+
+btnEnv.addEventListener("click", () => {
+  let vuelta1: number = Number(dato1.value);
+  let vuelta2: number = Number(dato2.value);
+  let vuelta3: number = Number(dato3.value);
+  let vuelta4: number = Number(dato4.value);
+  let resultado: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+  let promedio: number = resultado / 4;
+
+  console.log("El tiempo total es:", resultado);
+  console.log("El promedio de vuelta es de:", promedio);
+});
